34df5c76b15ab636e1e74deaa7ff321e740749e2
Mauro Campos <38911137+mauro-pinheiro@users.noreply.github.com>
2019-12-20 10:17:45 -0300

    Correções

--- questoes/1.js
+++ questoes/1.js
@@ -1,23 +1,23 @@
-document.querySelector('#addButton').addEventListener('click', function (event) {
-    event.preventDefault();
-    let number = document.querySelector('#input').value;
-    let table = document.querySelector('#output');
-    let row = document.createElement('tr');
-    let numberData = document.createElement('td');
-    let squareData = document.createElement('td');
-    let buttonData = document.createElement('td');
-    let removeButton = document.createElement('button');
+document.querySelector("#addButton").addEventListener("click", function(event) {
+  event.preventDefault();
+  let number = document.querySelector("#input").value;
+  let table = document.querySelector("#output");
+  let row = document.createElement("tr");
+  let numberData = document.createElement("td");
+  let squareData = document.createElement("td");
+  let buttonData = document.createElement("td");
+  let removeButton = document.createElement("button");
 
-    numberData.textContent = number;
-    squareData.textContent = number * number;
-    removeButton.innerHTML = '<i class="fas fa-ban"></i>';
-    removeButton.addEventListener('click', function () {
-        alert("Ainda não sei fazer em js");
-    });
+  numberData.textContent = number;
+  squareData.textContent = number * number;
+  removeButton.innerHTML = '<i class="fas fa-ban"></i>';
+  removeButton.addEventListener("click", function() {
+    alert("Ainda não sei fazer em js");
+  });
 
-    buttonData.append(removeButton);
-    row.append(numberData);
-    row.append(squareData);
-    row.append(buttonData);
-    table.append(row);
+  buttonData.append(removeButton);
+  row.append(numberData);
+  row.append(squareData);
+  row.append(buttonData);
+  table.append(row);
 });
--- questoes/2.js
+++ questoes/2.js
@@ -1,47 +1,47 @@
 function atualizaContagem() {
     /* let idades = document.querySelector('#output')
-        .rows
-        .map(function (r) { r.cells[0].textContent })
-
-    let maior = idades.filter(function (i) { i >= 18 }).length;
-    let menor = idades.filter(function (i) { i < 18 }).length; */
+          .rows
+          .map(function (r) { r.cells[0].textContent })
+  
+      let maior = idades.filter(function (i) { i >= 18 }).length;
+      let menor = idades.filter(function (i) { i < 18 }).length; */
 
     let maior = 0;
     let menor = 0;
 
-    let idades = document.querySelector('#output').rows;
+    let idades = document.querySelector("#output").rows;
     for (i = 0; i < idades.length; i++) {
         if (idades[i].cells[0].textContent >= 18) maior++;
         else menor++;
     }
 
-    document.querySelector('#conta-maior-18').value = maior;
-    document.querySelector('#conta-menor-18').value = menor;
+    document.querySelector("#maior-18").textContent = maior;
+    document.querySelector("#menor-18").textContent = menor;
 }
 
-//document.querySelector('#conta-maior-18').value = 0;
-//document.querySelector('#conta-menor-18').value = 0;
+document.querySelector("#maior-18").textContent = 0;
+document.querySelector("#menor-18").textContent = 0;
 
-document
-    .querySelector("#addButton")
-    .addEventListener('click', function (event) {
-        event.preventDefault();
-        let idade = document.querySelector("#input").value;
+document.querySelector("#addButton").addEventListener("click", function (event) {
+    event.preventDefault();
+    let idade = document.querySelector("#input").value;
 
-        let table = document.querySelector("#output");
-        let row = document.createElement('tr');
-        let idadeData = document.createElement('td');
-        let btnRemoveData = document.createElement('td');
-        let btnRemove = document.createElement('button');
+    let table = document.querySelector("#output");
+    let row = document.createElement("tr");
+    let idadeData = document.createElement("td");
+    let btnRemoveData = document.createElement("td");
+    let btnRemove = document.createElement("button");
 
-        idadeData.textContent = idade;
-        btnRemove.innerHTML = '<i class="fas fa-ban"></i>';
-        btnRemove.addEventListener('click', function (event) { alert("Ainda nao sei fazer em js.") });
+    idadeData.textContent = idade;
+    btnRemove.innerHTML = '<i class="fas fa-ban"></i>';
+    btnRemove.addEventListener("click", function (event) {
+        alert("Ainda nao sei fazer em js.");
+    });
 
-        btnRemoveData.append(btnRemove);
-        row.append(idadeData);
-        row.append(btnRemoveData);
-        table.append(row);
+    btnRemoveData.append(btnRemove);
+    row.append(idadeData);
+    row.append(btnRemoveData);
+    table.append(row);
 
-        atualizaContagem();
-    })
+    atualizaContagem();
+});
